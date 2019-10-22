bbb5e24bf788f50de72ba9cf9a394ee3b24a9d27
J <jmrumingan@gmail.com>
2019-10-22 19:51:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,12 @@
 def reverse_each_word(sentence)
   
   a_sentence = sentence.split
-  puts a_sentence
-  
+  a_reverse = []
   a_sentence.each do |i|
     
     puts i.reverse 
+    a_reverse.push(i.reverse)
   end 
+  
+  return a_reverse 
 end 
